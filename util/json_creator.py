727adfe38d9736ec965be6b09133af2cd3aa21df
psw1023 <psw1023@unist.ac.kr>
2023-02-23 16:16:55 +0900

    modifying a few things

--- util/json_creator.py
+++ util/json_creator.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('C:/Users/psw/OneDrive - UNIST/coding practice file/quiz web proj/util/jpvoca_230221.csv')
-file_output = open('C:/Users/psw/OneDrive - UNIST/coding practice file/quiz web proj/jpvoca/jpvoca.txt', 'w', encoding='utf-8')
+file_output = open('C:/Users/psw/OneDrive - UNIST/coding practice file/quiz web proj/jpvoca_data/jpvoca.txt', 'w', encoding='utf-8')
 
-for i in range(1, len(data)):
+for i in range(0, len(data)):
     file_output.writelines(["{\"일본어\": \"", str(data["한자"][i]), "\", \"후리가나\": \"", str(data["후리가나"][i]), "\", \"한국어\": [\"", str(data["뜻"][i]), "\"], \"품사\": \"", str(data["품사"][i]), "\", \"등록일자\": \"", str(data["등록일자"][i]), "\"},", "\n"])
     
 file_output.close()
